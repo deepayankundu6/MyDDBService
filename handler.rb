24bd8617b28f3feeaea27d0497223a7c3b1b3481
Deepayan Kundu <deepayankundu6@gmail.com>
2023-07-02 20:06:38 +0530

    fixes

--- handler.rb
+++ handler.rb
@@ -2,11 +2,33 @@ require 'json'
 require 'aws-sdk-dynamodb'
 
 def save_to_ddb(event:, context:)
+  
+  table_name = ENV['table_name']
+  payload = JSON.parse( event['body'] )
+  year = Time.new
+  id = rand(0..1000000000)
+
+  table_item = {
+      year: year.year.to_s,
+      unique_id: id.to_s,
+      body: payload
+  }
+  
+  ddb_service = Aws::DynamoDB::Client.new
+
+  ddb_service.put_item(
+    {
+      table_name: table_name,
+      item: table_item
+    }
+  )
+
   {
     statusCode: 200,
     body: {
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
+      message: 'Saved Successfully',
+      items: payload
     }.to_json
   }
+
 end
