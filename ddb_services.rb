4f44634e2158f48a99aec2070c4673439341457e
Deepayan Kundu <deepayankundu6@gmail.com>
2024-02-03 12:52:48 +0530

    updates to the APi added get endpoints

---
+++ ddb_services.rb
@@ -0,0 +1,89 @@
+require 'aws-sdk-dynamodb'
+
+def save_to_ddb(table_name, body)
+  puts "Saving the item into dynamo DB"
+  begin
+    year = Time.new
+    id = rand(0..1000000000)
+    ddb_client = Aws::DynamoDB::Client.new
+
+    table_item = {
+      table_name: table_name,
+        item: {
+          year:  year.year.to_s,
+          unique_id: id.to_s,
+          body: body
+        }
+    }
+
+    ddb_client.put_item(table_item)
+  rescue => error
+      puts "Error occured while saving the item in DDB: #{error}"
+      raise error
+  end
+end
+
+def get_item_ddb(table_name, item_pk, item_sk)
+puts "Getting item with keys as #{item_pk}:#{item_sk} from the table."
+  begin
+    ddb_client = Aws::DynamoDB::Client.new
+
+    table_item = {
+      table_name: table_name,
+      key: {
+        year: "#{item_pk}",
+        unique_id: "#{item_sk}"
+       }
+    }
+    response = ddb_client.get_item(table_item)
+    response["item"]
+
+  rescue => error
+      puts "Error occured while getting item from DDB: #{error}"
+      raise error
+  end
+end
+
+def get_items_ddb(table_name, item_pk)
+ puts "Getting items with key as #{item_pk} from the table."
+  begin
+    ddb_client = Aws::DynamoDB::Client.new
+
+    table_item = {
+      table_name: table_name,
+      select: "ALL_ATTRIBUTES",
+      scan_filter: {
+        year: {
+          attribute_value_list: ["#{item_pk}"],
+          comparison_operator: "EQ"
+        }
+      }
+    }
+
+    response = ddb_client.scan(table_item)
+    response["items"]
+
+  rescue => error
+      puts "Error occured while getting item from DDB: #{error}"
+      raise error
+  end
+end
+
+def get_ddb_table_keys(table_name)
+  puts "Getting partition keys of the table."
+  begin
+    ddb_client = Aws::DynamoDB::Client.new
+
+    table_item = {
+      table_name: table_name,
+      attributes_to_get: ["year","unique_id" ]
+      }
+
+    response = ddb_client.scan(table_item)
+    response["items"]
+
+  rescue => error
+      puts "Error occured while getting item from DDB: #{error}"
+      raise error
+  end
+end
--- handler.rb
+++ handler.rb
@@ -1,34 +1,32 @@
 require 'json'
-require 'aws-sdk-dynamodb'
+require './ddb_services'
 
-def save_to_ddb(event:, context:)
-  
-  table_name = ENV['table_name']
-  payload = JSON.parse( event['body'] )
-  year = Time.new
-  id = rand(0..1000000000)
+def my_ddb_service(event:, context:)
+  begin
+    table_name = ENV['table_name']
+    payload = JSON.parse( event['body'] ) if event['body']
+    path = event["path"]
 
-  table_item = {
-      year: year.year.to_s,
-      unique_id: id.to_s,
-      body: payload
-  }
-  
-  ddb_service = Aws::DynamoDB::Client.new
+    save_to_ddb(table_name, payload) if path.include? "save"
+    payload = get_item_ddb(table_name, payload["year"], payload["id"]) if path.include? "getItem"
+    payload = get_items_ddb(table_name, payload["year"]) if path.include? "items"
+    payload = get_ddb_table_keys(table_name) if path.include? "getKeys"
 
-  ddb_service.put_item(
     {
-      table_name: table_name,
-      item: table_item
+      statusCode: 200,
+      body: {
+        message: 'Executed Successfully',
+        items: payload
+      }.to_json
     }
-  )
 
-  {
-    statusCode: 200,
-    body: {
-      message: 'Saved Successfully',
-      items: payload
-    }.to_json
-  }
-
+  rescue => error
+    puts "Some error occured while executing the lambda: #{error}"
+    {
+      statusCode: 500,
+      body: {
+        message: error.message
+      }.to_json
+    }
+  end
 end
